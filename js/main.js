d23c329671113674b621639b98a7b3a29e360ca8
zile028 <zile028@gmail.com>
2021-08-22 13:51:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
